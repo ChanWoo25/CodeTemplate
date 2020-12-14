e1dcd9a49f525ae37702a398e77ddeb0d87d4897
Chanwoo Lee <50766175+ChanWoo25@users.noreply.github.com>
2020-12-14 21:01:24 +0900

    first

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "DLL.hpp"
+
+using namespace std;
+
+int main(){
+    
+    cout << "Hello World!" << endl;
+    cout << "2 + 3 = " << add(2, 3) << endl;
+    
+    return 0;
+}
